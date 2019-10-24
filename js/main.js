8e01d1b1888d88127461aa66f25f341af19da91c
Simone <simoneugazio1993@gmail.com>
2019-10-24 14:48:10 +0200

    2 push

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@
 //ma per i multipli di 3 stampi “Fizz” al posto del numero
 //e per i multipli di 5 stampi Buzz.
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+//stampo numeri da 1 a 100
+for (var i = 0; i <=100; i++){
+  console.log(i);
+  if (i * 3 = "Fizz"){
+    console.log(i);
+  }
+}
